9cf10c478182ed072aa428d3bfa29829c3b62b5c
Lucas_Nog <lucasemanuelndq@gmail.com>
2025-10-19 22:17:14 -0300

    Documentação Alunos Ok

--- src/main/java/br/com/LucasNog21/School/controller/StudentController.java
+++ src/main/java/br/com/LucasNog21/School/controller/StudentController.java
@@ -1,6 +1,7 @@
 package br.com.LucasNog21.School.controller;
 
 
+import br.com.LucasNog21.School.controller.docs.StudentControllerDocs;
 import br.com.LucasNog21.School.dto.StudentDTO;
 import br.com.LucasNog21.School.model.Student;
 import br.com.LucasNog21.School.service.StudentServices;
@@ -9,17 +10,17 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.print.attribute.standard.Media;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/alunos")
-public class StudentController {
+public class StudentController implements StudentControllerDocs {
 
     @Autowired
     private StudentServices service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @Override
     public List<StudentDTO> findAll(@RequestParam(value="curso", required = false) Long courseId) {
         if (courseId != null) {
             return service.findByCourseId(courseId);
@@ -29,12 +30,14 @@ public class StudentController {
 
     @GetMapping(value="/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
+    @Override
     public StudentDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
+    @Override
     public StudentDTO create(@RequestBody StudentDTO studentDTO) {
         Student student = service.create(studentDTO);
         return new StudentDTO(student);
@@ -42,11 +45,13 @@ public class StudentController {
 
     @PutMapping(value="/{id}", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
+    @Override
     public StudentDTO update(@PathVariable("id") Long id ,@RequestBody StudentDTO studentDTO) {
         return service.update(id, studentDTO);
     }
 
     @DeleteMapping(value = "/{id}")
+    @Override
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
 
         service.delete(id);
---
+++ src/main/java/br/com/LucasNog21/School/controller/docs/StudentControllerDocs.java
@@ -0,0 +1,94 @@
+package br.com.LucasNog21.School.controller.docs;
+
+import br.com.LucasNog21.School.dto.StudentDTO;
+import br.com.LucasNog21.School.model.Student;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+
+public interface StudentControllerDocs {
+    @Operation(summary = "Encontrar todos os alunos", description = "Encontrar todos os alunos", tags = {"Alunos"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = {
+                    @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(schema = @Schema(implementation = StudentDTO.class))
+                    )
+            }),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
+    List<StudentDTO> findAll(Long id);
+
+    @Operation(summary = "Encontrar um aluno por Id", description = "Encontrar um aluno por Id", tags = {"Alunos"}, responses = {
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content(schema = @Schema(implementation = StudentDTO.class))),
+            @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+            @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+    })
+    StudentDTO findById(Long id);
+
+    @Operation(summary = "Criar um novo aluno",
+            description = "Adicionar um aluno passando uma representação JSON do objeto",
+            tags = {"Alunos"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = StudentDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    StudentDTO create(@RequestBody StudentDTO student);
+
+    @Operation(summary = "Atualiza um aluno",
+            description = "Atualiza um aluno passando uma representação JSON do objeto",
+            tags = {"Alunos"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = StudentDTO.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    StudentDTO update(@PathVariable("id") Long id, @RequestBody StudentDTO student);
+
+    @Operation(summary = "Deleta um aluno",
+            description = "Deleta um aluno específico por ID",
+            tags = {"Alunos"},
+            responses = {
+                    @ApiResponse(
+                            description = "No Content",
+                            responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    ResponseEntity<?> delete(@PathVariable("id") Long id);
+}
+
