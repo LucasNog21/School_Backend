a63a1c2671afc7063bd969c6f1d9f35be98bdb9a
Lucas_Nog <lucasemanuelndq@gmail.com>
2025-10-20 22:38:40 -0300

    Documentação de usuário e autenticação OK - erro no refresh

--- src/main/java/br/com/LucasNog21/School/controller/AuthController.java
+++ src/main/java/br/com/LucasNog21/School/controller/AuthController.java
@@ -11,7 +11,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@Tag(name = "Endpoint de autenticação")
 @RestController
 @RequestMapping("/auth")
 public class AuthController implements AuthControllerDocs {
--- src/main/java/br/com/LucasNog21/School/security/jwt/JwtTokenProvider.java
+++ src/main/java/br/com/LucasNog21/School/security/jwt/JwtTokenProvider.java
@@ -103,6 +103,7 @@ public class JwtTokenProvider {
     public String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
 
+        //Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJsZWFuZHJvIiwicm9sZXMiOlsiQURNSU4iLCJNQU5BR0VSIl0sImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6ODA4MCIsImV4cCI6MTY1MjcxOTUzOCwiaWF0IjoxNjUyNzE1OTM4fQ.muu8eStsRobqLyrFYLHRiEvOSHAcss4ohSNtmwWTRcY
         if(refreshTokenContainsBearer(bearerToken)) return bearerToken.substring("Bearer ".length());
         return null;
     }
