12e94b3d9ab6e5335c9d87358ac4574ecb88d0aa
Lucas_Nog <lucasemanuelndq@gmail.com>
2025-10-30 16:08:14 -0300

    Adição Lombok CourseDTO

--- src/main/java/br/com/LucasNog21/School/dto/CourseDTO.java
+++ src/main/java/br/com/LucasNog21/School/dto/CourseDTO.java
@@ -1,44 +1,22 @@
 package br.com.LucasNog21.School.dto;
 
 import br.com.LucasNog21.School.model.Course;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.BeanUtils;
 
-
+@Getter @Setter
+@NoArgsConstructor
 public class CourseDTO {
 
     private Long id;
     private String name;
     private String code;
 
-    public CourseDTO() {
-
-    }
-
     public CourseDTO(Course course) {
         BeanUtils.copyProperties(course, this);
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
 }
