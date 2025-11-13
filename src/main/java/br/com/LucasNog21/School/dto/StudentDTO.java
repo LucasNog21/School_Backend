7c0c7cffd7ca6900f903205145021c4d069064a8
Lucas_Nog <lucasemanuelndq@gmail.com>
2025-11-13 16:52:54 -0300

    tudo testado e funcionando

--- src/main/java/br/com/LucasNog21/School/dto/StudentDTO.java
+++ src/main/java/br/com/LucasNog21/School/dto/StudentDTO.java
@@ -2,14 +2,12 @@ package br.com.LucasNog21.School.dto;
 
 import br.com.LucasNog21.School.model.Student;
 import br.com.LucasNog21.School.model.Subject;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.BeanUtils;
 
 import java.util.List;
-import java.util.Objects;
 
 @Getter @Setter
 @NoArgsConstructor
